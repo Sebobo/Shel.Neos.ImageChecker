05e841cf0ddb2bb7ebb85d995918c0ea9e9b0f83
Sebastian Helzle <sebastian@helzle.it>
2022-11-14 10:39:57 +0100

    BUGFIX: Prevent „null“ values in dimensions check for SVGs

--- Resources/Private/ImageChecker/src/Checks/filedimensions.ts
+++ Resources/Private/ImageChecker/src/Checks/filedimensions.ts
@@ -8,16 +8,22 @@ export function checkFileDimensions(
     options: { maxWidth: number; maxHeight: number; minWidth: number; minHeight: number },
     translate: TranslateMethod
 ): Promise<CheckResult> {
+    let { width, height } = dimensions;
+
+    if (!width) {
+        width = 0;
+    }
+    if (!height) {
+        height = 0;
+    }
+
     const maxWidth = options?.maxWidth || MAX_HEIGHT;
     const maxHeight = options?.maxHeight || MAX_WIDTH;
     const minWidth = options?.minWidth || MIN_HEIGHT;
     const minHeight = options?.minHeight || MIN_WIDTH;
 
-    const smallEnough =
-        (dimensions.width === 0 || dimensions.width <= maxWidth) &&
-        (dimensions.height === 0 || dimensions.height <= maxHeight);
-
-    const bigEnough = dimensions.width >= minWidth && dimensions.height >= minHeight;
+    const smallEnough = (width === 0 || width <= maxWidth) && (height === 0 || height <= maxHeight);
+    const bigEnough = width >= minWidth && height >= minHeight;
 
     const errorMessage = translate(
         'checks.dimensions.error',
@@ -32,7 +38,7 @@ export function checkFileDimensions(
 
     return Promise.resolve({
         isValid: smallEnough && bigEnough,
-        value: `${dimensions.width} x ${dimensions.height}px`,
+        value: width || height ? `${width} x ${height}px` : 'n/a',
         errorMessage: smallEnough && bigEnough ? '' : errorMessage,
     });
 }
