4f7d37455a0012df20585f2d6a13cd3941876205
Sebastian Helzle <sebastian@helzle.it>
2022-11-18 09:52:08 +0100

    BUGFIX: Prevent potential caching issues when retrieving image filesize

--- Resources/Private/ImageChecker/src/Checks/filesize.ts
+++ Resources/Private/ImageChecker/src/Checks/filesize.ts
@@ -12,59 +12,64 @@ export function checkFileSize(
     options: FileSizeCheckOptions,
     translate: TranslateMethod
 ): Promise<CheckResult> {
-    return fetch(url, { method: 'HEAD' }).then(async (response) => {
-        if (response.ok) {
-            const fileType = response.headers.get('Content-Type');
-            let fileSize = parseInt(response.headers.get('Content-Length')) || 0;
+    return fetch(url, { method: 'HEAD', cache: 'no-cache' })
+        .then(async (response) => {
+            if (response.ok) {
+                const fileType = response.headers.get('Content-Type');
+                let fileSize = parseInt(response.headers.get('Content-Length')) || 0;
 
-            // For SVGs we don't get the actual size via a HEAD request, so we need to fetch the whole file
-            if (!fileSize && fileType === 'image/svg+xml') {
-                const blob = await fetch(url, { method: 'GET' }).then((response) => response.blob());
-                fileSize = blob.size;
+                // For SVGs we don't always get the actual size via a HEAD request, so we need to fetch the whole file
+                if (!fileSize && fileType === 'image/svg+xml') {
+                    const blob = await fetch(url, { method: 'GET', cache: 'no-cache' }).then((response) =>
+                        response.blob()
+                    );
+                    fileSize = blob.size;
+                }
+
+                let maxSize = options.default || MAX_FILE_SIZE;
+                switch (fileType) {
+                    case 'image/png':
+                        maxSize = options.png || maxSize;
+                        break;
+                    case 'image/jpeg':
+                        maxSize = options.jpg || maxSize;
+                        break;
+                    case 'image/svg+xml':
+                        maxSize = options.svg || maxSize;
+                        break;
+                }
+
+                // fileSize is measured in bytes, maxSize is defined in KB
+                const isValid = fileSize <= maxSize * 1024;
+
+                let displayFileSize = fileSize;
+                let unit = 'B';
+                if (displayFileSize > 1024) {
+                    displayFileSize /= 1024;
+                    unit = 'KB';
+                }
+                if (displayFileSize > 1024) {
+                    displayFileSize /= 1024;
+                    unit = 'MB';
+                }
+
+                return {
+                    isValid,
+                    value: `${Math.round(displayFileSize)} ${unit}`,
+                    errorMessage: isValid
+                        ? ''
+                        : translate('checks.fileSize.error', `File size must be less or equal than ${maxSize} KB`, {
+                              maxSize,
+                          }),
+                };
             }
-
-            let maxSize = options.default || MAX_FILE_SIZE;
-            switch (fileType) {
-                case 'image/png':
-                    maxSize = options.png || maxSize;
-                    break;
-                case 'image/jpeg':
-                    maxSize = options.jpg || maxSize;
-                    break;
-                case 'image/svg+xml':
-                    maxSize = options.svg || maxSize;
-                    break;
-            }
-
-            // fileSize is measured in bytes, maxSize is defined in KB
-            const isValid = fileSize <= maxSize * 1024;
-
-            let displayFileSize = fileSize;
-            let unit = 'B';
-            if (displayFileSize > 1024) {
-                displayFileSize /= 1024;
-                unit = 'KB';
-            }
-            if (displayFileSize > 1024) {
-                displayFileSize /= 1024;
-                unit = 'MB';
-            }
-
+            throw new Error();
+        })
+        .catch(() => {
             return {
-                isValid,
-                value: `${Math.round(displayFileSize)} ${unit}`,
-                errorMessage: isValid
-                    ? ''
-                    : translate('checks.fileSize.error', `File size must be less or equal than ${maxSize} KB`, {
-                          maxSize,
-                      }),
+                isValid: false,
+                value: 'n/a',
+                errorMessage: translate('checks.fileSize.fetchError', 'Error while fetching file size'),
             };
-        }
-
-        return {
-            isValid: false,
-            value: 'n/a',
-            errorMessage: translate('checks.fileSize.fetchError', 'Error while fetching file size'),
-        };
-    });
+        });
 }
